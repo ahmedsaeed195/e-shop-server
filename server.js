557f4eaded2fa9c95e48016a190afdf501479140
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-05-28 18:45:33 +0200

    fixing morgan import condition

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const Router = require('./routes/routes')
-const morgan = require('morgan')
 require('dotenv').config()
 
 const app = express()
 
 if (app.get('env') === 'development') {
+    const morgan = require('morgan')
     app.use(morgan('dev'))
 }
 app.use(express.json())
