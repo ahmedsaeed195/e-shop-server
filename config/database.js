d069eec28e8a1dbab267055ce3ead939c0ad0e28
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-05-29 16:25:23 +0200

    change DB with env

--- config/database.js
+++ config/database.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(process.env.DB_URI).then(() => console.log('Connected to MongoDB')).catch(err => console.error('Could not connect to MongoDB', err))
+const DB = () => {
+    if (process.env.NODE_ENV === 'Production') {
+        return process.env.DB_PROD
+    }
+    return process.env.DB_DEV
+}
+
+const DB_URI = `${process.env.DB_URI}${DB()}`
+
+mongoose.connect(DB_URI).then(() => console.log('Connected to MongoDB')).catch(err => console.error('Could not connect to MongoDB', err))
 
 module.exports = mongoose
